caf800f4355aaf5a93bf688d431614bce66c101f
chhengz <sokchheng802@gmail.com>
2025-07-04 21:15:01 +0700

    update

--- resources/views/book/master.blade.php
+++ resources/views/book/master.blade.php
@@ -39,12 +39,9 @@
     </div>
 </nav>
 
-
-
     <div class="container  my-5">
         @yield('content')
     </div>
-
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
--- routes/web.php
+++ routes/web.php
@@ -29,30 +29,6 @@ Route::get('/book/create', function () {
     return view('book.create');
 })->name('book.create');
 
-// // show a book
-// Route::get('/book/show', function () {
-//     return view('book.show');
-// })->name('book.show');
-
-// // edit book
-// Route::get('/book/edit/{id}', function ($id) {
-//     return view('book.edit', ['id' => $id]);
-// })->name('book.edit');
-
-
-// store a new book
-// Route::post('/book/store/', [BookController::class, 'store'])->name('book.store');
-// show a book
-// Route::get('/book/show', [BookController::class, 'show'])->name('book.show');
-// edit a book
-// Route::get('/book/edit/{id}', [BookController::class, 'edit'])->name('book.edit');
-// update a book
-// Route::post('/book/update/{id}', [BookController::class, 'update'])->name('book.update');
-// delete a book
-// Route::get('/book/delete/{id}', [BookController::class, 'delete'])->name('book.delete');
-// Route::delete('/book/delete/{id}', [BookController::class, 'delete'])->name('book.delete');
-
-
 // Auth Routes
 Route::get('/auth/login', [AuthController::class, 'login'])->name('auth.login');
 Route::get('/auth/register', [AuthController::class, 'register'])->name('auth.register');
